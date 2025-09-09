f68e4b81ccbab3a0d5b1d8e3a8895518fdc54cbe
Artis.A <A240523AA@rvt.lv>
2025-09-09 13:40:28 +0300

    Pirmais uzdevums"Message"

---
+++ src/main/java/rvt/Message.java
@@ -0,0 +1,17 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Message {
+    
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("AAA");
+
+            String input = reader.nextLine();
+
+            System.out.println(input);
+            reader.close();
+    }
+}
