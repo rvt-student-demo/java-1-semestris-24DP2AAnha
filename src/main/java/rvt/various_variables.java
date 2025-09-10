0a68adaa87f316ffdd32ac64433ad05dd7c14a74
Artis.A <A240523AA@rvt.lv>
2025-09-10 13:09:33 +0300

    Various variables

---
+++ src/main/java/rvt/various_variables.java
@@ -0,0 +1,19 @@
+package rvt;
+
+public class various_variables {
+    public static void main(String[] args){
+        System.out.println("chicken");
+        int janis = 3;
+        System.out.print(janis);
+        System.out.println("Bacon(kg)");
+        double pi = 5.5f;
+        System.out.println(pi);
+        System.out.println("Tractor:");
+        System.out.println("None!");
+        System.out.println("And finnaly, a summary:)");
+        System.out.print(janis);
+        System.out.println(pi);
+        System.out.println("None!");
+    }
+
+}
---
+++ src/main/java/rvt/various_variables1.java
@@ -0,0 +1,21 @@
+package rvt;
+
+public class various_variables1 {
+    public static void main(String[] args){
+        int janis = 9000;
+        double pi = 0.1f;
+
+        System.out.println("chicken");
+        System.out.print(janis);
+        System.out.println("Bacon(kg)");
+        System.out.println(pi);
+        System.out.println("Tractor:");
+        System.out.println("Zetor");
+        System.out.println("");
+        System.out.println("And finnaly, a summary:)");
+        System.out.print(janis);
+        System.out.println(pi);
+        System.out.println("Zetor");
+    }
+
+}
